64c948f609c0114e39e7dc59f8d5bc55a9733c63
조수빈 <angelajsb@naver.com>
2024-01-11 16:30:33 +0900

    01/11 java test method prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application03.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application03.java
@@ -1,14 +1,21 @@
 package com.ohgiraffers.section01.method;
 
 public class Application03 {
-    /*  */
+    static int global = 10;                        // 전역변수이자 클래스 변수 (static 안 붙으면 전역변수)
+
+    /* 수업목표. 메소드 전달 인자와 매개변수에 대해 이해하고 메소드 호출 시 활용 */
     /* 필기.
      *  변수의 종류 -> 자료형과는 다른 구분방식
      *  1. 지역변수 2. 매개변수 3. 전역변수(field) 4. 클래스(static) 변수
     * */
     public static void main(String[] args) {
+
+        int global = 20;                        // 지역변수 (우선순위)
+        System.out.println("global -> "+global);
+        System.out.println("global -> "+Application03.global);  // 클래스명. 변수명을 하면 클래스 변수 호출 가능
+
         Application03 app3 = new Application03();
-        app3.testMethod(10);
+        app3.testMethod(10);                // 10 -> 전달인자
         app3.testMethod(19);
         app3.testMethod(15);
         app3.testMethod('a');
