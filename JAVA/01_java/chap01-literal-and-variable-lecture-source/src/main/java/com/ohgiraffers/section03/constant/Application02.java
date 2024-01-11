ab6dc0bcf5317db14b3b97f3cf963d7539e53e91
조수빈 <angelajsb@naver.com>
2024-01-11 10:06:52 +0900

    01/11 java consant prac

--- JAVA/01_java/chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section03/constant/Application02.java
+++ JAVA/01_java/chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section03/constant/Application02.java
@@ -2,6 +2,27 @@ package com.ohgiraffers.section03.constant;
 
 public class Application02 {
     public static void main(String[] args) {
-        /* 수업목표.  */
+        /* 수업목표. */
+        /* 목차. 1) 모든 문자는 영문자 대문자 혹은 숫자만 사용 (특수기호 X) */
+        final int AGE1 = 20;
+        final int AGE2 = 30;
+        final int age= 40;              // 소문자 사용 가능하지만 변수와 구분 힘들어 지양
+
+        /* 목차. 2) 단어와 단어 연결은 언더스코어(_) 사용 */
+        final int MAX_AGE = 60;
+        final int MIN_AGE = 20;
+        final int maxAge = 30;          // camel 형태 가능하지만 변수와 구분 힘들어 지양
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ JAVA/01_java/chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section04/overflow/Application01.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section04.overflow;
+
+public class Application01 {
+    public static void main(String[] args) {
+        
+    }
+}
