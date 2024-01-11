8ad5bfeca20f8b4ef86d91307641017a6fefc9ca
조수빈 <angelajsb@naver.com>
2024-01-11 15:14:34 +0900

    01/11 java method prac

---
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application01.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01.method;
+
+public class Application01 {
+    public static void main(String[] args) {
+        /* 수업목표. 메소드의 호출 흐름에 대해 이해 */
+        /* 필기.
+         *  메소드(method) -> 특정 작업을 수행하기 위한 명령문의 집합
+        * */
+        System.out.println("main() 시작");
+    }
+
+    public void methodA() {
+        System.out.println("methodA call");
+    }
+}
