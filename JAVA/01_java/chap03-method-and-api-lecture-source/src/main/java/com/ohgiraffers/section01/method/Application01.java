8e6f0af3bd89e69f3dc236474fc684d90d7cc236
조수빈 <angelajsb@naver.com>
2024-01-11 15:27:29 +0900

    01/11 java method prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application01.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application01.java
@@ -5,11 +5,27 @@ public class Application01 {
         /* 수업목표. 메소드의 호출 흐름에 대해 이해 */
         /* 필기.
          *  메소드(method) -> 특정 작업을 수행하기 위한 명령문의 집합
-        * */
+         * */
         System.out.println("main() 시작");
+        methodA();
+        System.out.println("main() 종료");
     }
 
-    public void methodA() {
-        System.out.println("methodA call");
+    public static void methodA() {          // 단순하게 호출할 경우 static 필수
+        System.out.println("methodA() 시작");
+        methodB();
+        System.out.println("methodA() 종료");
+
+    }
+
+    public static void methodB() {
+        System.out.println("methodB() 시작");
+        methodC();
+        System.out.println("methodB() 종료");
+    }
+
+    public static void methodC() {
+        System.out.println("methodC() 시작");
+        System.out.println("methodC() 종료");
     }
 }
---
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application02.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section01.method;
+
+public class Application02 {
+    public static void main(String[] args) {
+        System.out.println("main() 시작");
+        System.out.println("main() 종료");
+    }
+    public static void methodA() {
+        System.out.println("methodA() 시작");
+        System.out.println("methodA() 종료");
+    }
+    public static void methodB() {
+        System.out.println("methodB() 시작");
+        System.out.println("methodB() 종료");
+    }
+    public static void methodC() {
+            System.out.println("methodC() 시작");
+            System.out.println("methodC() 종료");
+    }
+}
