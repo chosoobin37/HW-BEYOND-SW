d9d0d40a1f367f116eb1e6ab2ebab3e33ed1b899
조수빈 <angelajsb@naver.com>
2024-01-11 15:43:49 +0900

    01/11 java method  new prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application01.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application01.java
@@ -2,7 +2,7 @@ package com.ohgiraffers.section01.method;
 
 public class Application01 {
     public static void main(String[] args) {
-        /* 수업목표. 메소드의 호출 흐름에 대해 이해 */
+        /* 수업목표. 메소드의 호출 흐름에 대해 이해 (메소드 호출 시 내부에서 다른 메소드 재호출) */
         /* 필기.
          *  메소드(method) -> 특정 작업을 수행하기 위한 명령문의 집합
          * */
--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application02.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application02.java
@@ -2,14 +2,23 @@ package com.ohgiraffers.section01.method;
 
 public class Application02 {
     public static void main(String[] args) {
+        /* 수업목표. 메소드의 호출 흐름에 대해 이해 (main 메소드에서 한 번에 순차적으로 여러 메소드 호출) */
+        /* 설명.
+         *  static이 붙어 있지 않은 메소드는 <클래스명 변수명 = new.클래스명();> -> 접근연산자(.)도 활용
+        * */
+
         System.out.println("main() 시작");
+        Application02 app2 = new Application02();
+        app2.methodA();             // static 없이 호출하는 경우
+        app2.methodB();
+        methodC();
         System.out.println("main() 종료");
     }
-    public static void methodA() {
+    public void methodA() {          // 단순 호출 시 static 없으면 main에서 호출 불가능 (methodA가 안 보임)
         System.out.println("methodA() 시작");
         System.out.println("methodA() 종료");
     }
-    public static void methodB() {
+    public void methodB() {
         System.out.println("methodB() 시작");
         System.out.println("methodB() 종료");
     }
