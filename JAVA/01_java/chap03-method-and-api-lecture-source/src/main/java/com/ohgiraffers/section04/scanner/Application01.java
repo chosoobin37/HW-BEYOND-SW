98d75ffb2637695ade8dc38960718a3a2d6c219c
조수빈 <angelajsb@naver.com>
2024-01-12 11:45:38 +0900

    01/12 java System.in prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application01.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application01.java
@@ -1,4 +1,30 @@
 package com.ohgiraffers.section04.scanner;
 
+import java.util.Scanner;
+
 public class Application01 {
+    public static void main(String[] args) {
+
+        /* 수업목표. java.util.Scanner를 이용한 다양한 자료형 값 입력받기 */
+//        java.util.Scanner sc= new java.util.Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
+
+        /* 목차. 1) 문자열 입력 받기 */
+        System.out.println("이름을 입력하세요: ");
+        String name = sc.nextLine();
+        System.out.println("입력하신 이름은: "+name);
+
+        /* 목차. 2) 정수형 입력 받기 */
+
+
+        /* 목차. 3) 실수형 입력 받기 */
+
+
+        /* 목차. 4) 논리형 입력 받기 */
+
+
+        /* 목차. 5) 문자형 입력 받기 */
+
+
+    }
 }
