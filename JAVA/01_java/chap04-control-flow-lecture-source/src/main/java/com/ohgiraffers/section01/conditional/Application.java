4f84b87b51a14242bc206885c0ee3163d02ef09c
조수빈 <angelajsb@naver.com>
2024-01-12 14:08:22 +0900

    01/12 java if prac

---
+++ JAVA/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Application.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.conditional;
+/* 설명. 실행용 클래스 구분 -> main method only here */
+public class Application {
+    public static void main(String[] args) {
+
+        A_if aClass = new A_if();
+
+        /* 필기. 단독 if문 흐름 확인용 메소드 호출 */
+//        aClass.testSimpleIfStatement();
+
+        /* 필기. 중첩 if문 흐름 확인용 메소드 호출 */
+        aClass.testNestedIfStatement();
+    }
+}
