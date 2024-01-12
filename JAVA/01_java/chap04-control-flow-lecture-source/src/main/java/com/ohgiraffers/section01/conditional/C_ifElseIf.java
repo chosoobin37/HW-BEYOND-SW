ffbd05a9aee05020613e5049eb8d0cbc1f461001
조수빈 <angelajsb@naver.com>
2024-01-12 15:39:41 +0900

    01/12 java student grade prac

--- JAVA/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/C_ifElseIf.java
+++ JAVA/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/C_ifElseIf.java
@@ -16,15 +16,16 @@ public class C_ifElseIf {
         Scanner sc = new Scanner(System.in);
         int answer = sc.nextInt();
 
-        if (answer==1) {
+        if (answer == 1) {
             System.out.println("이런 거짓말쟁이 !! 너에게는 아무런 도끼도 줄 수 없다 !!");
-        } else if(answer==2) {
+        } else if (answer == 2) {
             System.out.println("욕심이 과하지는 않지만 그래도 거짓말을 하고 있구나 !!");
         } else {
             System.out.println("아주 정직하구나 여기 있는 모든 도끼들을 다 가져가거라 ~");
         }
         System.out.println("그렇게 산신령은 다시 연못 속으로 홀연히 사라지고 말았다 ,,");
     }
+
     public void testNestedIfElseIfStatement() {
 
         /* 수업목표. 중첩된 if-else-if문에 대한 흐름 이해 및 적용 */
@@ -36,37 +37,47 @@ public class C_ifElseIf {
          *  (추가로, 각 등급의 중간점수(95, 85, 75, ...) 이상인 경우 '+'를 붙여 등급 세분화)
          *  출력 예시: XXX 학생의 점수는 86점이고, 등급인 'B+'입니다.
          *           프로그램을 종료합니다.
-        * */
+         * */
 
         /* 수업목표. 중첩된 if-else문 단독 사용에 대한 흐름 이해 및 적용 */
 
+        /* 학생의 이름과 점수 입력 */
         Scanner sc = new Scanner(System.in);
-        System.out.print("이름을 입력하시오: ");
+        System.out.print("학생의 이름을 입력하세요 : ");
         String name = sc.nextLine();
-        System.out.print("점수를 입력하시오: ");
-        int score = sc.nextInt();
+        System.out.print("학생의 점수를 입력하세요 : ");
+        int point = sc.nextInt();
 
-        /* 설명. 정수를 하나 입력 받아서 양수와 음수 구분 뒤 양숭리 때만 짝수, 홀수 판별 (각 상태 모두 출력) */
-        if (score >= 95) {
-            System.out.println(name+" 학생의 점수는 "+score+"점이고, 등급은 A+입니다.");
-        } else if (score >= 90) {
-            System.out.println(name+" 학생의 점수는 "+score+"점이고, 등급은 A입니다.");
-        } else if (score >= 85) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 B+입니다.");
-        } else if (score >= 80) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 B입니다.");
-        } else if (score >= 75) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 C+입니다.");
-        } else if (score >= 70) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 C입니다.");
-        } else if (score >= 65) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 D+입니다.");
-        } else if (score >= 60) {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 D입니다.");
+        /* 학생의 등급을 저장하기 위한 변수 초기화 */
+        String grade = "";
+
+        /* 학점등급을 매기기 위해 점수를 확인 하기 위한 조건문 */
+        if (point >= 90) {
+            /* 점수가 90점 이상인 경우 */
+            grade = "A";
+        } else if (point >= 80) {
+            /* 점수가 90점 미만, 80점 이상인 경우 */
+            grade = "B";
+        } else if (point >= 70) {
+            /* 점수가 80점 미만, 70점 이상인 경우 */
+            grade = "C";
+        } else if (point >= 60) {
+            /* 점수가 70점 미만, 60점 이상인 경우 */
+            grade = "D";
         } else {
-            System.out.println(name + " 학생의 점수는 " + score + "점이고, 등급은 F입니다.");
+            /* 위 조건을 다 만족하지 못하는 점수가 60점 미만인 경우 */
+            grade = "F";
         }
+        /* 각 등급의 중간 점수 이상이면서 60점 이상인 경우인지 확인(즉, +를 붙이는 조건에 맞는지 확인) */
+        if (point % 10 >= 5 && point >= 60 || point == 100) {
+            /* 60점 이상이면서 10으로 나눈 나머지가 5보다 큰 경우 */
+            grade += "+";
+        }
+        /* 위에서 결정된 등급을 점수와 함께 출력한다. */
+        System.out.println(name + " 학생의 점수는 " + point + "이고, 등급은 " + grade + "입니다.");
         System.out.println("프로그램을 종료합니다.");
     }
 }
+
+
 
