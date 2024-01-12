0e2679ffd3e1d8efe90bd1aea09d2ea0d65f7f82
조수빈 <angelajsb@naver.com>
2024-01-12 16:06:01 +0900

    01/12 java switch prac

--- JAVA/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
+++ JAVA/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
@@ -15,8 +15,8 @@ public class D_switch {
             case 'G': point += 10;
             case 'S': point += 10;
             case 'B': point += 10;
-            default:
-                System.out.println("어디 회원 등급이세요? ");
+//            default:
+//               System.out.println("어디 회원 등급이세요? ");
         }
         System.out.println("당신의 등급은 "+grade+"이며, 현재 포인트는 "+point+"입니다.");
     }
