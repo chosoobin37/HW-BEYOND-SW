f9c555d5c3bd50cee4b49c32b4b368280ca3fec7
조수빈 <angelajsb@naver.com>
2024-01-12 10:49:10 +0900

    01/12 java import prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
@@ -1,11 +1,13 @@
 package com.ohgiraffers.section02.package_and_import;
 
-import com.ohgiraffers.section01.method.Calculator;
+import com.ohgiraffers.section01.method.Calculator; // Calculator라고 불러도 사용 가능
 
 public class Application02 {
     public static void main(String[] args) {
         /* 수업목표. import에 대해 이해 */
         Calculator cal = new Calculator();
+        int sub = cal.subTwoNumbers(100,50);
+        System.out.println("100 - 50 = "+sub);
 
     }
 }
