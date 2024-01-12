6018053aa1db97d67c1621f51950ea487f054a58
조수빈 <angelajsb@naver.com>
2024-01-12 10:44:11 +0900

    01/12 java import prac

--- JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
+++ JAVA/01_java/chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application02.java
@@ -1,7 +1,11 @@
 package com.ohgiraffers.section02.package_and_import;
 
+import com.ohgiraffers.section01.method.Calculator;
+
 public class Application02 {
     public static void main(String[] args) {
-        
+        /* 수업목표. import에 대해 이해 */
+        Calculator cal = new Calculator();
+
     }
 }
