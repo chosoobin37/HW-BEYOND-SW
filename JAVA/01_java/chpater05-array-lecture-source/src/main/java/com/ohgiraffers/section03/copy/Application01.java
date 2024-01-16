8e322443b98800c8c5c32257832f5fc006c74903
조수빈 <angelajsb@naver.com>
2024-01-16 10:24:21 +0900

    01/16 java shallow copy  prac

--- JAVA/01_java/chpater05-array-lecture-source/src/main/java/com/ohgiraffers/section03/copy/Application01.java
+++ JAVA/01_java/chpater05-array-lecture-source/src/main/java/com/ohgiraffers/section03/copy/Application01.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.section03.copy;
 
 public class Application01 {
+    public static void main(String[] args) {
+        /* 수업목표. 배열의 복사 개념 중 얕은 복사에 대해 이해 */ 
+        /* 필기.
+         *  배열의 복사 
+         *  1. 얕은 복사 (shallow copy) -> stack의 주소값만 복사
+         *  2. 깊은 복사 (deep copy) -> heap의 배열에 저장된 값 복사
+        * */
+    }
 }
