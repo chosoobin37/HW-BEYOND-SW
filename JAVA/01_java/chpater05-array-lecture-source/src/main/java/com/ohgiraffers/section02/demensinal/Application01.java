371452ff502b084fb1e887afbb90859c09dde578
조수빈 <angelajsb@naver.com>
2024-01-16 10:16:58 +0900

    01/16 java dimensinal array prac

--- JAVA/01_java/chpater05-array-lecture-source/src/main/java/com/ohgiraffers/section02/demensinal/Application01.java
+++ JAVA/01_java/chpater05-array-lecture-source/src/main/java/com/ohgiraffers/section02/demensinal/Application01.java
@@ -13,6 +13,7 @@ public class Application01 {
         int[][] iArr1;
         int[][] iArr2;
 
+        /* 설명. 정변배열은 이미 1차우너 배열들 생성 */
         iArr1 = new int[3][2];          // 2칸짜리 1차원 배열 3개 관리 (관리하는 1차원 배열의 길이 동일) -> 정변 배열
         iArr2 = new int[3][];           // 크기 현재 미정 (관리하는 1차원 배열의 길이 동일 X) -> 가변 배열
 
@@ -24,17 +25,14 @@ public class Application01 {
             System.out.println(Arrays.toString(iArr1[i]));
         }
 
+        /* 설명. 가변 배열은 아직 1차원 배열들이 생성 X -> NullPointerException 발생 가능 */
         int length = 1;
-        for (int i = 0; i < iArr2.length; i++) {                // 1차원 배열 고르는 for문
+        for (int i = 0; i < iArr2.length; i++) {
             iArr2[i] = new int[++length] ;
-            for (int j = 0; j < iArr2[i].length; j++) {         // 선택된 1차원 배열의 길이만큼 반복하는 for문
+            for (int j = 0; j < iArr2[i].length; j++) {
                 iArr2[i][j] = num++;
             }
             System.out.println(Arrays.toString(iArr2[i]));
         }
-
-//        iArr1[0]=new int[2];
-//        iArr1[1]=new int[3];
-
     }
 }
