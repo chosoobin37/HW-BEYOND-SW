a549c0df2ede871552ba82bfa455db3ba7dfb750
조수빈 <angelajsb@naver.com>
2024-01-16 12:40:14 +0900

    01/16 java class Monster2 prac

--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem2/Monster.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem2/Monster.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.section02.encapsulation.problem2;
 
 public class Monster {
+//    String name;
+//    int hp;
+
+    /* 설명. 필드의 변수명이 바뀐 경우  */
+    String kinds;
+    int mp;
+
+    // 단일책임의 원칙 -> 유지보수 용이
+    void setInfo1(String info1) {
+//        this.name = info1;
+        this.kinds = info1;
+    }
+
+    void setInfo2(int info2) {
+//        this.hp = info2;
+        this.mp = info2;
+    }
 }
