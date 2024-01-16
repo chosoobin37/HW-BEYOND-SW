a44a58f9ea711b4551b0cc16ad07bd665b706aad
조수빈 <angelajsb@naver.com>
2024-01-16 12:27:46 +0900

    01/16 java class Monster prac

--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem1/Application.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem1/Application.java
@@ -1,4 +1,30 @@
 package com.ohgiraffers.section02.encapsulation.problem1;
 
 public class Application {
+    public static void main(String[] args) {
+
+        /* 수업목표. 필드에 직접 접근하는 경우 발생하는 문제접 이해 */
+        Monster monster1 = new Monster();
+
+        /* 설명. 1번 몬스터 생성 */
+        monster1.name = "두치";           // 참조연산자(.)를 통한 값 대입
+        monster1.hp = 200;
+
+        /* 설명. 몬스터 정보 출력 */
+        System.out.println("monster1의 이름: " + monster1.name);   // 참조연산자를 통한 값 출력
+        System.out.println("monster1의 체력: " + monster1.hp);
+
+        /* 설명. 2번 몬스터 생성 */
+        Monster monster2 = new Monster();
+        monster2.name = "뿌꾸";
+        monster2.hp = -200;
+
+        System.out.println("monster2의 이름: " + monster2.name);
+        System.out.println("monster2의 체력: " + monster2.hp);
+
+        /* 설명. 3번 몬스터 생성(문제점 확인) */
+        Monster monster3 = new Monster();
+        monster3.name = "드라큘라";
+        monster3.setHP(100);        // monster3의 hp 공간에 100 할당
+    }
 }
--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem1/Monster.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem1/Monster.java
@@ -1,4 +1,13 @@
 package com.ohgiraffers.section02.encapsulation.problem1;
 
 public class Monster {
+    String name;
+    int hp;
+
+    public void setHP(int hp) {
+        this.hp = hp;
+        // this -> 메소드 호출 당시의 Monster 개체 의미, this.hp -> 해당 객체의 hp공간 의미
+        // 상황에 따라 this는 매번 달라짐
+
+    }
 }
