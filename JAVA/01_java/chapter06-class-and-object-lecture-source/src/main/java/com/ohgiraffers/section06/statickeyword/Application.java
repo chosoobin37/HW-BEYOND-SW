bc46d60c7163c8976930e02908110d94e38648c6
조수빈 <angelajsb@naver.com>
2024-01-17 12:26:36 +0900

    01/16 java StaticFieldTest prac

--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/statickeyword/Application.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/statickeyword/Application.java
@@ -7,5 +7,25 @@ public class Application {
          *  static -> 프로그램이 실행될 때 정적 메모리 영역(static or class 영역)에 할당하는 키워드
          *  여러 인스턴스가 공유해서 사용할 목적의 공간 ex) singleton 객체
         * */
+
+        /* 목차. 1) static 키워드를 필드에서 사용 */
+        StaticFieldTest stf1 = new StaticFieldTest();
+
+        /* 설명. 현재 두 필드가 가지고 있는 값 확인 */
+        System.out.println("non-static field: " + stf1.getNonStaticCount());
+        System.out.println("static field: " + StaticFieldTest.getStaticCount());
+
+        /* 설명. 각 필드값들을 하나씩 증가 */
+        stf1.increaseNonStaticCount();
+        StaticFieldTest.increaseStaticCount();
+
+        /* 설명. 두 필드값 확인 */
+        System.out.println("non-static field: " + stf1.getNonStaticCount());
+        System.out.println("static field: " + StaticFieldTest.getStaticCount());
+
+        /* 설명. 새로운 객체 생성 */
+        StaticFieldTest stf2 = new StaticFieldTest();
+        System.out.println("non-static field: " + stf2  .getNonStaticCount());
+        System.out.println("static field: " + StaticFieldTest.getStaticCount());
     }
 }
