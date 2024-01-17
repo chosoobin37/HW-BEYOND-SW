bc9b2dda5253c609e7ad4e4eb7d9ebe86afc5a68
조수빈 <angelajsb@naver.com>
2024-01-17 14:16:09 +0900

    01/16 java LazySingleton prac

--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/singleton/LazySingleton.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/singleton/LazySingleton.java
@@ -1,7 +1,15 @@
 package com.ohgiraffers.section06.singleton;
 
 public class LazySingleton {
+
+    private static LazySingleton lazy;
+
+    private LazySingleton() {}
     public static LazySingleton getInstance() {
-        return null;
+
+        if (lazy == null) {             // static 변수인 lazy에 한 번도 객체가 생성 및 할당된 적이 없다면
+            lazy = new LazySingleton();
+        }
+        return lazy;
     }
 }
