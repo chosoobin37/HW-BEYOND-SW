b9947a7778e3042226397041a4f8a8ae28f9c57b
조수빈 <angelajsb@naver.com>
2024-01-17 15:10:54 +0900

    01/16 java ObjectArray prac

---
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/object_array/Car.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section08.object_array;
+
+public class Car {
+
+    private String moduleName;
+    public int maxSpeed;
+
+    public Car() {
+        this.moduleName = moduleName;
+        this.maxSpeed = maxSpeed;
+
+    }
+
+    public Car(String moduleName, int maxSpeed) {
+    }
+
+    public void driveMaxSpeed() {
+        System.out.println(moduleName + "차량이 최고 시속 " + maxSpeed + "(km/h)로 달립니다!");
+    }
+}
