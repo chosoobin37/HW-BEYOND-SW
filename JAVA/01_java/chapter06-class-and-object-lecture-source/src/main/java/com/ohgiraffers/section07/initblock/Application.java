083bacdb602aa2d22b99d4775b0902a974258a50
조수빈 <angelajsb@naver.com>
2024-01-17 14:40:24 +0900

    01/16 java InitBlock prac

---
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section07/initblock/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section07.initblock;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 수업목표. 초기화블럭의 동작 순서를 이해 및 활용 인스턴스 생성*/
+        Product product = new Product();
+        Product product2 = new Product("샤오미");
+    }
+}
+
