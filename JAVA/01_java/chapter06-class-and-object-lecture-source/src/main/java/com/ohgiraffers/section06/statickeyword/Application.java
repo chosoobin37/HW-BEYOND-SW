c9aca4722208dc1fb42f61de5325f815457fa0b4
조수빈 <angelajsb@naver.com>
2024-01-17 12:09:15 +0900

    01/16 java Static Keyword prac

---
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/statickeyword/Application.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section06.statickeyword;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 수업목표. static keyword 이해 */
+        /* 필기.
+         *  static -> 프로그램이 실행될 때 정적 메모리 영역(static or class 영역)에 할당하는 키워드
+         *  여러 인스턴스가 공유해서 사용할 목적의 공간 ex) singleton 객체
+        * */
+    }
+}
