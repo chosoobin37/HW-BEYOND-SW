1826922e0b833a0d8c7be3d04c5a25d6d36bd97c
조수빈 <angelajsb@naver.com>
2024-01-17 11:03:21 +0900

    01/16 java Parameter prac

---
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section05.parameter;
+
+import java.lang.reflect.Parameter;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 수업목표. 메소드의 파라미터 이해 및 사용 */
+        /* 필기.
+         *  매개변수(parameter)로 사용 가능한 자료형
+         *  1. 기본자료형 2. 기본자료형 배열 3. 클래스자료형 4. 클래스자료형 배열 5. 가변인자
+        * */
+
+        ParameterTest pt = new ParameterTest();
+    }
+}
