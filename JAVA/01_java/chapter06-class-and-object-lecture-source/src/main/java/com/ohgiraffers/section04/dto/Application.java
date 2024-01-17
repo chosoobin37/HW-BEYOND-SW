e36af9068e33f0039e201cffee9ddae2850f2fea
조수빈 <angelajsb@naver.com>
2024-01-17 11:48:18 +0900

    01/16 java ParameterTest prac

--- JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
+++ JAVA/01_java/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
@@ -7,10 +7,10 @@ public class Application {
 
         /* 수업목표. 생성자를 이용한 객체 초기화와 설정자를 이용한 초기화의 장단점 이해 */
         UserDTO user1 = new UserDTO();
-        System.out.println(user1.forInformation());
+//        System.out.println(user1.forInformation());
         user1.setId("user01");
         System.out.println(user1.getId());
-        System.out.println(user1.forInformation());
+//        System.out.println(user1.forInformation());
 
     }
 }
