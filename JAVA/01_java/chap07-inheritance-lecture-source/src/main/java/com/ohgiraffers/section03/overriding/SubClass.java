f14d4eb21c5747b0eb4b9ccb2acb50884e75a7df
조수빈 <angelajsb@naver.com>
2024-01-18 11:46:57 +0900

    01/18 java Inheritance Suepr-Sub Class prac

--- JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SubClass.java
+++ JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SubClass.java
@@ -1,4 +1,38 @@
 package com.ohgiraffers.section03.overriding;
 
-public class SubClass {
+public class SubClass extends SuperClass {
+    
+    /* 설명. 0) 부모 메소드와 이름, 반환형, 매개변수, 접근제어자 모두 동일 -> 오버라이딩 성립 */
+    @Override
+    public void method(int num) {}
+
+    /* 설명. 1) 메소드 이름 변경(error) */
+//    @Override
+//    public void method(int num) {}
+
+    /* 설명. 2) 메소드 리턴 타입 변경(error) */
+//    @Override
+//    public String method(int num) {return null;}
+
+    /* 설명. 3) 메소드 매개변수 차이나게 변경(error) */
+//    @Override
+//    public void method(int num, int num2) {}
+
+    /* 설명. 4) 경우에 따라 반환형이 달라도 가능 (부모 메소드 반환형의 자식 타입은 가능) */
+    @Override
+//    public String method2(int num2) {return null;}
+    public Integer method2(int num2) {return null;}
+
+    /* 설명. 5) private 메소드 -> 오버라이딩 불가 */
+//    @Override
+//    private void privateMethod() {}
+
+    /* 설명. 6) final 메소드 -> 오버라이딩 불가 */
+//    @Override
+//    private void finalMethod() {}
+
+    /* 설명. 7) 부모 타입과 같은 접근제어자 or 더 넓은 범위의 접근제어자 -> 오버라이딩 가능 */
+    @Override
+    public void protectedMethod() {}
+
 }
--- JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SuperClass.java
+++ JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SuperClass.java
@@ -1,4 +1,22 @@
 package com.ohgiraffers.section03.overriding;
 
 public class SuperClass {
+
+    /* 수업목표. OverRiding 이해 */
+
+    /* 필기. Override 성립 조건
+     *  1. 메소드의 이름이 동일해야 함
+     *  2. 메소드의 반환형(return type)이 동일해야 함 (이후 수정사항 있음)
+     *  3. 매개변수의 타입, 갯수, 순서가 동일해야 함 (변수명 상관X)
+     *  4. 부모 클래스의 private 메소드는 자식 클래스가 접근이 불가능 -> 오버라이딩 불가능
+     *  5. final 키워드가 사용된 메소드 -> 오버라이딩 불가능
+     *  6. 접근 제어자 -> 부모 메소드와 같거나 더 넓은 범위여야 함
+     *  7. 예외처리는 같은 예외이거나 더 구체적(하위)인 범위의 예외를 처리
+    * */
+
+    public Object method2 (int num) {return null;}
+    public void method(int num) {}
+    private void privateMethod() {}
+    public final void finalMethod() {}
+    protected void protectedMethod() {}
 }
