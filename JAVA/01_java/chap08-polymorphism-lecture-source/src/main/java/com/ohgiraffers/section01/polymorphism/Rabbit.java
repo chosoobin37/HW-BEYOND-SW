a387f64326d26f58c64ab2b440ce93025971dac1
조수빈 <angelajsb@naver.com>
2024-01-18 12:42:58 +0900

    01/18 java Polymorphism Animal prac

--- JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Rabbit.java
+++ JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Rabbit.java
@@ -1,4 +1,20 @@
 package com.ohgiraffers.section01.polymorphism;
 
 public class Rabbit extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("토끼가 풀을 뜯어먹습니다. ");
+    }
+
+    public void run() {
+        System.out.println("토끼가 달립니다. 깡총 ~ 깡총 ~ ");
+    }
+
+    public void cry() {
+        System.out.println("토끼가 울음소리를 냅니다. 끼잉 ~ ");
+    }
+
+    public void jump() {
+        System.out.println("토끼가 점프합니다. 짬푸 !! 짬푸 !!");
+    }
 }
--- JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Tiger.java
+++ JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Tiger.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.section01.polymorphism;
 
 public class Tiger extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("호랑이가 고기를 뜯어 먹습니다. ");
+    }
+
+    public void run() {
+        System.out.println("호랑이는 웬만해서 달리지 않습니다. ");
+    }
+
+    public void cry() {
+        System.out.println("호랑이가 울부짖습니다. 어흥 ~ ");
+    }
+
+    /* 설명. 호랑이는 물어뜯기 추가 */
+    public void bite() {
+        System.out.println("호랑이가 물어뜯습니다. 앙 ~");
+    }
 }
