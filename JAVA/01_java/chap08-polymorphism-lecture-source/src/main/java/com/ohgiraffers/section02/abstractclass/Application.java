dfd83b79f305f0bb24e953fec312b4cca45d331c
조수빈 <angelajsb@naver.com>
2024-01-18 15:09:55 +0900

    01/18 java Abstract prac

--- JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/abstractclass/Application.java
+++ JAVA/01_java/chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/abstractclass/Application.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.section02.abstractclass;
 
 public class Application {
     public static void main(String[] args) {
-        
+
+        /* 수업목표. 추상클래스 및 추상메소드 이해 */
+
     }
 }
