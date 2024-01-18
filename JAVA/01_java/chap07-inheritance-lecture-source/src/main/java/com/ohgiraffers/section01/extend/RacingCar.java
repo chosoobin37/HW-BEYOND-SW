1ed70ef8c57e3ca1f3de60cfdec06ce555b56f1e
조수빈 <angelajsb@naver.com>
2024-01-18 10:27:57 +0900

    01/18 java Extends prac

--- JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section01/extend/RacingCar.java
+++ JAVA/01_java/chap07-inheritance-lecture-source/src/main/java/com/ohgiraffers/section01/extend/RacingCar.java
@@ -3,6 +3,10 @@ package com.ohgiraffers.section01.extend;
 public class RacingCar extends Car {
     @Override
     public void run() {
-        super.run();
+        System.out.println("레이싱 카 경주 시작. 쌩 ~");
+    }
+    @Override
+    public void soundHorn() {
+        System.out.println("레이싱 모드에는 경적 기능이 없습니다.");
     }
 }
