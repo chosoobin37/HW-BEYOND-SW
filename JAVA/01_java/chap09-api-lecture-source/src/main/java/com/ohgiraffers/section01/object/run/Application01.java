2e21fb99586967d42bf97cf595b68d30b69f21ff
조수빈 <angelajsb@naver.com>
2024-01-19 10:23:08 +0900

    01/19 java Object API prac

---
+++ JAVA/01_java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/run/Application01.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section01.object.run;
+
+public class Application01 {
+    public static void main(String[] args) {
+        
+        /* 수업목표. Object 클래스의 toString() 메소드 오버라이딩 목적 이해 및 활용 */ 
+    }
+}
