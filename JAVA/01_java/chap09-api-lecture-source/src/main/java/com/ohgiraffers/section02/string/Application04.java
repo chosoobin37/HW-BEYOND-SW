7739a0ea415492d4fc5f8f46490daa5c4bcbf222
조수빈 <angelajsb@naver.com>
2024-01-19 14:21:20 +0900

    01/19 java Escape prac

--- JAVA/01_java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section02/string/Application04.java
+++ JAVA/01_java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section02/string/Application04.java
@@ -13,5 +13,19 @@ public class Application04 {
          *  \": 큰 따옴표
          *  \\: 역슬래쉬 표시
         * */
+
+        System.out.println("안녕하세요.\n 저는 홍길동입니다.");
+        System.out.println("안녕하세요.\t 저는 홍길동입니다.");
+
+        System.out.println("안녕하세요. 저는 '홍길동'입니다.");
+        System.out.println("\'");
+        System.out.println("안녕하세요. 저는 \"홍길동\"입니다.");
+
+        System.out.println("역슬래쉬(\\)입니다.");
+
+        /* 설명. escape 문자 이외도 printf 관련 문법 */ 
+        System.out.printf("원주율은 %.2f입니다. " +
+                "우린 %d로 하죠. ", 3.141592, 3);
+
     }
 }
