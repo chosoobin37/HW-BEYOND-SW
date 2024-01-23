32b7962a0a9c96a149acb7617a576249b8fc4b7f
조수빈 <angelajsb@naver.com>
2024-01-23 11:51:04 +0900

    01/22 Java Collection HashSet

--- JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Application01.java
+++ JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Application01.java
@@ -1,4 +1,23 @@
 package com.ohgiraffers.section02.set.run;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application01 {
+    public static void main(String[] args) {
+        
+        /* 수업목표. Set 자료구조 특성 이해 및 hashSet 이용 */
+//        HashSet<String> hset = new HashSet<>();
+        Set<String> hset = new HashSet<>();
+        hset.add(new String("java"));
+        hset.add(new String("mariaDB"));
+        hset.add(new String("servlet"));
+        hset.add(new String("spring"));
+        hset.add(new String("html"));
+
+        System.out.println("hset: " + hset);
+
+        hset.add(new String("mariaDB"));
+        System.out.println("hset: " + hset);
+    }
 }
