4438620a3581a2677425d0d57e2d3c98e20cecc4
조수빈 <angelajsb@naver.com>
2024-01-23 17:06:39 +0900

    01/22 Java Collection Lotto assginment

--- JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Assignment.java
+++ JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Assignment.java
@@ -1,8 +1,31 @@
 package com.ohgiraffers.section02.set.run;
 
+import java.util.Arrays;
+
 public class Assignment {
     public static void main(String[] args) {
 
-    }
+        int[] lotto = new int[6];
 
+        for (int i = 0; i < lotto.length; i++) {
+            lotto[i] = (int)((Math.random()) * 45) + 1;
+            for (int j = 0; j < i; j++) {
+                if (lotto[j] == lotto[i]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < lotto.length; i++) {
+            for (int j = 0; j < lotto.length - 1; j++) {
+                if (lotto[j] > lotto[j+1]) {
+                    int temp = lotto[j];
+                    lotto[j] = lotto[j + 1];
+                    lotto[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Lotto 번호는: " + Arrays.toString(lotto));
+    }
 }
