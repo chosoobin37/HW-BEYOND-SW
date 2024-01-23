e4105ee378ca29171d4cb1d2a55647c124cdc5b6
조수빈 <angelajsb@naver.com>
2024-01-23 13:55:04 +0900

    01/22 Java Collection TreeSet

--- JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Application03.java
+++ JAVA/01_java/chap11-collection-lecture-source/src/main/java/com/ohgiraffers/section02/set/run/Application03.java
@@ -17,5 +17,18 @@ public class Application03 {
 
         System.out.println("tSet = " + tSet);
 
+        /* 설명.
+        *   과제 -> 중복되지 않는 번호를 발생시켜 로또 번호 발생기 만들기 (TreeSet 없이) */
+
+        Set<Integer> lotto = new TreeSet<>();
+
+        /* 설명. 1부터 45까지의 6자리 중복되지 않는 난수 발생 및 값 저장 */ 
+        while (lotto.size() < 6) {
+            lotto.add((int) (Math.random() * 45) + 1);
+        }
+
+        /* 설명. 출력 시에는 오름차순으로 출력되도록 작성 */ 
+        System.out.println("lotto: " + lotto);
+
     }
 }
