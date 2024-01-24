6dfadf9b1d7737eaf51dcfa6d4069bec65be13f7
조수빈 <angelajsb@naver.com>
2024-01-24 09:50:14 +0900

    01/24 Java UserException PriceNegativeException

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
@@ -1,9 +1,17 @@
 package com.ohgiraffers.section02.userexception;
 
+import com.ohgiraffers.section02.userexception.exception.PriceNegativeException;
+
 public class Application01 {
     public static void main(String[] args) {
 
         /* 수업목표. 사용자 정의의 예외클래스 정의 후 발생한 사용자 정의의 예외 처리 */
         ExceptionTest et = new ExceptionTest();
+
+        try {
+            et.checkEnoughMoney(30000,50000);
+        } catch (PriceNegativeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
