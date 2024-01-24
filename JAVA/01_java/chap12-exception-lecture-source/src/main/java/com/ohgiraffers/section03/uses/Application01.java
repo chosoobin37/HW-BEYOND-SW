430b748241f45bf8353131a1e8bd1f7155ab1de8
조수빈 <angelajsb@naver.com>
2024-01-24 11:27:57 +0900

    01/24 Java UserException TryWithResource

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application01.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application01.java
@@ -27,13 +27,28 @@ public class Application01 {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            
+            /* 필기.
+            *   예외처리 구문과 상관없이 반드시 수행해야 하는 경우 -> finally에 작성
+            *   보통 사용한 자원을 반납할 목적으로 사용 */
             try {
-                br.close();
+                /* 설명. 
+                 *  스트림이 생성되지 않았을 때 접근해서 close() 메소드 호출 -> NullPointerException 발생 가능
+                 *  따라서, NullPointerException 방지 위한 if문 활용
+                * */
+                if (br != null) {
+
+                    /* 설명.
+                     *  입출력에서 사용한 스트림을 닫아주는 메소드
+                     *  API에서 확인하면 IOException을 thorws 하는 메소드 ->
+                     *  finally 블럭 안이어도 또 예외 처리 중첩으로 해야하함
+                    * */
+                    br.close();
+                }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
-
 
     }
 }
---
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application02.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section03.uses;
+
+import java.awt.image.BufferStrategy;
+import java.io.*;
+
+public class Application02 {
+    public static void main(String[] args) {
+
+        /* 수업목표. try-with-resource 구문 문법 이해 및 활용 */
+        try (BufferedReader br = new BufferedReader(new FileReader("test.dat"))) {
+            String s=;
+
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidObjectException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
