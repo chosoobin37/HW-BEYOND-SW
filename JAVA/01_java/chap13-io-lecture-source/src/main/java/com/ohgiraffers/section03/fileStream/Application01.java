06203a5b729a1ddf5c79237824a991bc2201f553
조수빈 <angelajsb@naver.com>
2024-01-24 15:23:55 +0900

    01/24 Java IO fileStream

--- JAVA/01_java/chap13-io-lecture-source/src/main/java/com/ohgiraffers/section03/fileStream/Application01.java
+++ JAVA/01_java/chap13-io-lecture-source/src/main/java/com/ohgiraffers/section03/fileStream/Application01.java
@@ -1,8 +1,7 @@
 package com.ohgiraffers.section03.fileStream;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.Buffer;
 
 public class Application01 {
     public static void main(String[] args) {
@@ -10,23 +9,52 @@ public class Application01 {
         /* 필기.
          *  내부적으로 버퍼 활용해서 입출력 성능 향상
          *  추가적인 메소드 제공
-        * */
+         * */
 
         BufferedWriter bw = null;
         try {
-            bw=new BufferedWriter(
+            bw = new BufferedWriter(
                     new FileWriter("src/main/java/com/ohgiraffers/section03/fileStream/testBuffered.txt"));
 
-                bw.write("드디어 푸바오가 당근을 먹을 수 있어 ");
+            bw.write("드디어 푸바오가 당근을 먹을 수 있어 \n");
+            bw.write("드디어 푸바오가 워토우를 먹을 수 있어\n");
+            bw.write("푸바오는 사과를 제일 좋아해 ~ ");
+
+            /* 설명.
+             *  버퍼를 이용해서 출력을 하는 경우 -> 버퍼 공간이 가득 차지 않으면 내보내기(출력)가 안 되는 경우 존재
+             *  -> 버퍼에 담긴 내용을 강제로 내보내기 위해 flush() 활용
+             *  (write를 했는데 파일에 값이 안 적혀있으면 flush() 활용)
+             *  -
+             *  stream을 close()하면 내부적으로 flush() 호출하긴 함
+             * */
+            bw.flush();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             try {
-                if(bw!=null) bw.close();
+                if (bw != null) bw.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+        /* 설명. BufferedWriter를 활용한 한 줄씩 출력하기 */
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(
+                    new FileReader("src/main/java/com/ohgiraffers/section03/fileStream/testBuffered.txt"));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+        } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+        try {
+            if (br != null) br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+            }
     }
 }
