4d15dd07646391a9d59b243842aa15b9a48cf2ee
조수빈 <angelajsb@naver.com>
2024-01-24 11:13:04 +0900

    01/24 Java UserException Uses IO

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application01.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application01.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section03.uses;
 
+import java.io.*;
+
 public class Application01 {
     public static void main(String[] args) {
 
@@ -8,5 +10,30 @@ public class Application01 {
          *  예외처리를 가장 많이 사용하게 되는 상황에서 try-catch 구문 실제 상황과 유사하게 연습
          *  문법보다 호출 흐름에 포커스
         * */
+        System.out.println(new File("test.dat").getAbsoluteFile());
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("test1.dat"));
+
+            String s="";
+//            System.out.println(br.readLine());
+
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("파일 못 찾겠어잉");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
     }
 }
