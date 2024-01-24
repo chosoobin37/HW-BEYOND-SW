6ce539596ec23c1ce5f52777582a996a36d04e2e
조수빈 <angelajsb@naver.com>
2024-01-24 10:18:05 +0900

    01/24 Java UserException Price

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.userexception;
 
+import com.ohgiraffers.section02.userexception.exception.MoneyNegativeException;
+import com.ohgiraffers.section02.userexception.exception.NotEnoughMoneyException;
 import com.ohgiraffers.section02.userexception.exception.PriceNegativeException;
 
 public class Application01 {
@@ -9,9 +11,18 @@ public class Application01 {
         ExceptionTest et = new ExceptionTest();
 
         try {
-            et.checkEnoughMoney(30000,50000);
-        } catch (PriceNegativeException e) {
-            throw new RuntimeException(e);
+//            et.checkEnoughMoney(-30000, 50000);
+//            et.checkEnoughMoney(30000, -50000);
+            et.checkEnoughMoney(300000, 50000);
+//        } catch (PriceNegativeException e) {
+//            System.out.println(e.getMessage());
+//        } catch (NotEnoughMoneyException e) {
+//            System.out.println(e.getMessage());
+//        } catch (MoneyNegativeException e) {
+//            System.out.println(e.getMessage());
+        } 
+        catch (Exception e) {           // 다형성을 활용한 catch 처리방식
+            System.out.println("예외 메시지: " + e.getMessage());
         }
     }
 }
