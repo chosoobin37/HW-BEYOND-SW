3fbfe6bea132b64709b2d7a10cb9b648026bed52
조수빈 <angelajsb@naver.com>
2024-01-25 08:48:28 +0900

    01/24 Java IO fileStream

---
+++ JAVA/01_java/chap13-io-lecture-source/src/main/java/com/ohgiraffers/section03/fileStream/Application02.java
@@ -0,0 +1,46 @@
+package com.ohgiraffers.section03.fileStream;
+
+import java.io.*;
+
+public class Application02 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 표준입출력 이해 및 활용 */
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("문자열 입력: ");
+        try {
+            String value=br.readLine();
+
+            System.out.println("value: "+value);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        BufferedWriter bw = null;
+        OutputStreamWriter osw = null;
+        osw = new OutputStreamWriter(System.out);
+        bw = new BufferedWriter(osw);
+
+        try {
+            bw.write("println 구웃");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
