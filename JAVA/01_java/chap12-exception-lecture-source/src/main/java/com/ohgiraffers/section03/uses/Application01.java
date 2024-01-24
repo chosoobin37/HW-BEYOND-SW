15a8814df34b79ac326ec8a2b308466f16720ec7
조수빈 <angelajsb@naver.com>
2024-01-24 10:45:10 +0900

    01/24 Java UserException Uses IO

---
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section03/uses/Application01.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section03.uses;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 예외처리를 많이 사용하는 io 패키지에서 사용하는 구문 이해 */
+        /* 설명.     
+         *  예외처리를 가장 많이 사용하게 되는 상황에서 try-catch 구문 실제 상황과 유사하게 연습
+         *  문법보다 호출 흐름에 포커스
+        * */
+    }
+}
