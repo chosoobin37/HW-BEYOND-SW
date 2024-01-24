57feff575d051c4e88d26522c8810a56d336e7d8
조수빈 <angelajsb@naver.com>
2024-01-24 14:35:13 +0900

    01/24 Java IO Stream

---
+++ JAVA/01_java/chap13-io-lecture-source/src/main/java/com/ohgiraffers/section02/stream/Application04.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section02.stream;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application04 {
+    public static void main(String[] args) {
+        
+        /* 수업목표. FileWriter 이해 및 활용 */
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("src/main/java/com/ohgiraffers/section02/stream/testWriter.txt");
+            fw.write('한');
+            fw.write('글');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(fw!=null) fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
