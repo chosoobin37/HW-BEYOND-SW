88a15d177e35a448cfe38c129a0eccd6ec537ee8
조수빈 <angelajsb@naver.com>
2024-01-24 09:42:29 +0900

    01/24 Java UserException

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
@@ -4,5 +4,6 @@ public class Application01 {
     public static void main(String[] args) {
 
         /* 수업목표. 사용자 정의의 예외클래스 정의 후 발생한 사용자 정의의 예외 처리 */
+        ExceptionTest et = new ExceptionTest();
     }
 }
