b02d703d50b9a3ec65b73602bfae3e4db5755e35
조수빈 <angelajsb@naver.com>
2024-01-24 10:35:57 +0900

    01/24 Java UserException MultiCatch

--- JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application03.java
+++ JAVA/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application03.java
@@ -1,4 +1,8 @@
 package com.ohgiraffers.section02.userexception;
 
 public class Application03 {
+    public static void main(String[] args) {
+        
+        /* 수업목표. multi-catch 구문 이해 및 활용 */ 
+    }
 }
