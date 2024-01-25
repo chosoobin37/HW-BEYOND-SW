fe1f78c0c431debab05a2f1f7fac29afc6f1be29
조수빈 <angelajsb@naver.com>
2024-01-25 11:38:02 +0900

    01/25 Java IntEnum01/25 Java EnumType

--- JAVA/01_java/chap14-enum-lecture-source/src/main/java/com/ohgiraffers/section02/enumtype/Subjects.java
+++ JAVA/01_java/chap14-enum-lecture-source/src/main/java/com/ohgiraffers/section02/enumtype/Subjects.java
@@ -6,5 +6,16 @@ public enum Subjects {
     JDBC,               // 2
     HTML,               // 3
     CSS,                // 4
-    JAVASCRIPT          // 5
+    JAVASCRIPT,
+    ;      // 5
+
+    Subjects() {
+        System.out.println("기본 생성자 호출");
+    }
+
+    @Override
+    public String toString() {
+        return "@@@" + this.name() + "@@@";
+    }
+
 }
