14ad24e04a487479c112b527d9447908513c5a96
조수빈 <angelajsb@naver.com>
2024-01-26 11:49:20 +0900

    01/26 Java Lambda Interface

--- JAVA/01_java/chap15-lambda-lecture-source/src/main/java/com/ohgiraffers/section01/intro/OuterCalculator.java
+++ JAVA/01_java/chap15-lambda-lecture-source/src/main/java/com/ohgiraffers/section01/intro/OuterCalculator.java
@@ -1,4 +1,20 @@
 package com.ohgiraffers.section01.intro;
 
 public interface OuterCalculator {
+    @FunctionalInterface
+    public interface Sum {
+        int sumTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Minus {
+        int sumTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Multiply {
+        int sumTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Divide {
+        int sumTwoNumber(int a, int b);
+    }
 }
