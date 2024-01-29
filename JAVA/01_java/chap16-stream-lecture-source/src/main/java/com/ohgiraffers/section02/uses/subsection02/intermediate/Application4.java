06617fb365b379118b518cdd6f94d6c15125c213
조수빈 <angelajsb@naver.com>
2024-01-29 09:35:01 +0900

    01/29 Java Stream Intermediate

--- JAVA/01_java/chap16-stream-lecture-source/src/main/java/com/ohgiraffers/section02/uses/subsection02/intermediate/Application4.java
+++ JAVA/01_java/chap16-stream-lecture-source/src/main/java/com/ohgiraffers/section02/uses/subsection02/intermediate/Application4.java
@@ -1,4 +1,19 @@
 package com.ohgiraffers.section02.uses.subsection02.intermediate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Application4 {
+    public static void main(String[] args) {
+        
+        /* 수업목표. 스트림의 중계연산 중 하나인 sorted 이해 및 사용 */
+        List<Integer> integerList = IntStream.of(5, 10, 99, 2, 1, 35)
+                                            .boxed()
+                                            .sorted()
+                                            .collect(Collectors.toList());
+
+        System.out.println("정렬된 Integer List: " + integerList);
+
+    }
 }
