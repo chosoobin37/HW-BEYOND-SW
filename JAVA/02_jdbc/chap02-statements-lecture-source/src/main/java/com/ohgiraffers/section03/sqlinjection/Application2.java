820cdeaec15327306d7eb705b36bb179b6727a8f
조수빈 <angelajsb@naver.com>
2024-02-05 14:22:20 +0900

    02/05 JDBC statement

--- JAVA/02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application2.java
+++ JAVA/02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application2.java
@@ -8,6 +8,11 @@ import java.sql.SQLException;
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
+/* 설명.
+ *  Statement와 달리 placeholder(?)를 통한 sql injection 공격을 막을 수 있게 작성되어 있어
+ *  보안적 측면에서도 PreparedStatement가 우수
+* */
+
 public class Application2 {
     private static String empId = "200";
     private static String empName = "' OR 1=1 AND EMP_ID='200";
@@ -18,6 +23,10 @@ public class Application2 {
         ResultSet rset = null;
 
         String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ? AND EMP_NAME=?";
+
+        /* 설명. 아래 SQL과 같이 PreparedStatement는 placeholder로 입력되는 값에 single quotation(')이 있다면
+        *   single quotation을 하나 더 붙여주고 setString으로 값이 들어간다면 양 옆에도 single quotation 추가 작성   */
+//        SELECT * FROM EMPLOYEE WHERE EMP_ID='200'AND EMP_NAME=''' OR 1=1 AND EMP_ID = ''200';
 
         try {
             pstmt = con.prepareStatement(query);
