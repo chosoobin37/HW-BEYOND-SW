1ef12a5382d700a9872f5cebbe02f53b28ce0d0f
조수빈 <angelajsb@naver.com>
2024-02-13 21:24:19 +0900

    02/13 BaekJoon Algorithm

---
+++ Baekjoon_Algorithm/baekjoon_1417.py
@@ -0,0 +1,20 @@
+num=int(input())
+arr=[]
+
+for i in range(num):
+    arr.append(int(input()))
+dasom=arr[0]
+dasom_out=arr[1:]
+
+ppl_count=0
+dasom_out.sort()
+
+if num == 1:
+    print(0)
+else:
+    while dasom<max(dasom_out):
+        dasom_out[-1] -= 1
+        dasom += 1
+        ppl_count += 1
+        dasom_out.sort()
+    print(ppl_count)
