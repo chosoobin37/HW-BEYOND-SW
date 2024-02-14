70af7c580d916368474746a44fcbe02a246908dc
조수빈 <angelajsb@naver.com>
2024-02-14 10:49:35 +0900

    02/14 Spring Bean Scope

---
+++ 04_spring/chpa05-bean-scope-lecture-source/src/main/java/com/ohgiraffers/common/Beverage.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.common;
+
+public class Beverage extends Product {
+    private int capacity;       // 용량
+
+    public Beverage() {
+    }
+    public Beverage(String name, int price) {
+        super(name, price);
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Beverage{" +
+                "capacity=" + capacity +
+                "} " + super.toString();
+    }
+}
---
+++ 04_spring/chpa05-bean-scope-lecture-source/src/main/java/com/ohgiraffers/common/ShoppingCart.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private final List<Product> items;
+
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
+}
