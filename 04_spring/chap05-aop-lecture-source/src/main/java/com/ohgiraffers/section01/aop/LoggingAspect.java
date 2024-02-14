34623698d6693b131d83517f9035a2322de12fb7
조수빈 <angelajsb@naver.com>
2024-02-14 15:37:22 +0900

    02/14 Spring AOP

---
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section01.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(* com.ohgiraffers.section01.aop.*Service.*(..))")
+    public void logPointCut() {}
+
+//    @Before("execution(* com.ohgiraffers.section01.aop.*Service.*(..))")
+    @Before("LoggingAspect.logPointCut()")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Before Advice 동작 ...");
+    }
+}
