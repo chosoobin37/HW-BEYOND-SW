a1a48ab4b686195aa6a4259ddd5f381923337d72
조수빈 <angelajsb@naver.com>
2024-02-16 09:49:19 +0900

    02/16 SpringBoot Handler Method

--- 05_springboot/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ 05_springboot/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Controller
 /* 설명. 현재의 Controller 클래스에 작성할 핸들러 메소드들이
@@ -25,19 +28,59 @@ public class FirstController {
     }
 
     @PostMapping("regist")
-    
-    /* 설명. request를 쓰게 된다면 request 개념은 사용자의 입력값이 존재, model은 동적 페이지의 재료를 담는 용도로 사용 */ 
+
+    /* 설명. request를 쓰게 된다면 request 개념은 사용자의 입력값이 존재, model은 동적 페이지의 재료를 담는 용도로 사용 */
 //    public String registMenu(HttpServletRequest request) {
     public String registMenu(WebRequest request, Model model) {
         String name = request.getParameter("name");
         System.out.println("name = " + name);
 
+        // Servlet
         int price = Integer.valueOf(request.getParameter("price"));
         int categoryCode = Integer.valueOf(request.getParameter("categoryCode"));
 
         String message
                 = name + "을(를) 신규 메뉴 목록의 " + categoryCode + "번 카테고리에 "
                 + price + "원으로 등록하였습니다.";
+        model.addAttribute("message", message);
+
+        return "first/messagePrinter";
+    }
+
+    @GetMapping("modify")
+    public void modify() {
+    }
+
+    /* 설명. request의 parameter로 넘어오는 값들의 key 값과 일치하는 변수명 작성하고 @RequestParam 어노테이션 적용
+     *   -> 자동으로 값을 꺼내고 해당 매개변수의 자료형에 맞게 변환 처리까지 완료, @RequestParam 생략 가능
+     *   Spring부터는 Controller의 파싱 작업 간소화 */
+
+    /* 설명.
+     *  @RequestParam 속성
+     *  1) defaultValue -> 사용자의 입력값이 없거나("") 
+     *                      request의 parameter 키 값과 일치하지 않는 매개변수 명 사용 시 매개변수가 가질 default 값 작성
+     *  2) name -> request parameter의 키 값과 다른 매개변수 명 사용하고 싶을 경우 request parameter의 키 값 작성
+    * */
+
+    // SpringBoot
+    // @RequestParam 생략 가능 (Model model, String name, int modifyPrice
+    @PostMapping("modify")
+    public String modifyMenu(Model model,
+                             @RequestParam(defaultValue = "default value", name = "name") String name,
+                             @RequestParam(defaultValue = "0") int modifyPrice) {
+        String message = name + " 메뉴의 가격을 " + modifyPrice + "원으로 변경하였습니다.";
+        model.addAttribute("message", message);
+
+        return "first/messagePrinter";
+    }
+
+    @PostMapping("modify2")
+    public String modifyMenu(Model model, @RequestParam Map<String, String> parameter) {
+
+        String modifyName = parameter.get("name2");
+        int modifyPrice = Integer.valueOf(parameter.get("modifyPrice2"));
+
+        String message = modifyName + " 메뉴의 가격을 " + modifyPrice + "원으로 변경하였습니다.";
         model.addAttribute("message", message);
 
         return "first/messagePrinter";
