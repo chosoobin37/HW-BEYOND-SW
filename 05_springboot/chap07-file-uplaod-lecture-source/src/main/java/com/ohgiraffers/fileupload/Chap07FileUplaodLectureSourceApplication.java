4206b29aa63b4cb0dc103d8e5ebc5771fe7e733f
조수빈 <angelajsb@naver.com>
2024-02-19 18:35:49 +0900

    02/19 SpringBoot File Upload

---
+++ 05_springboot/chap07-file-uplaod-lecture-source/src/main/java/com/ohgiraffers/fileupload/Chap07FileUplaodLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap07FileUplaodLectureSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap07FileUplaodLectureSourceApplication.class, args);
+	}
+
+}
