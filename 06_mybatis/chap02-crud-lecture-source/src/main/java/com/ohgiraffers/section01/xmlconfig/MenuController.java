38c71f7a9c64216356ba1bb36a62e0fd3e35c5f8
조수빈 <angelajsb@naver.com>
2024-02-20 19:51:44 +0900

    02/20 MyBatis CRUD

---
+++ 06_mybatis/chap02-crud-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/MenuController.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+import java.util.Map;
+
+public class MenuController {
+    private final MenuService menuService;
+    private final PrintResult printResult;      // 결과 페이지에 해당하는(View 개념) 클래스
+    public MenuController() {
+        menuService = new MenuService();
+        printResult = new PrintResult();
+    }
+
+    public void findAllMenus() {
+        List<MenuDTO> menuList = menuService.findAllMenus();
+
+        if (!menuList.isEmpty()) {
+            printResult.printMenus(menuList);
+        } else {
+            printResult.printErrorMessage("조회할 메뉴가 없습니다.");
+        }
+    }
+
+    public void findMenuByMenuCode(Map<String, String> parameter) {
+
+        int menuCode = Integer.valueOf(parameter.get("menuCode"));
+        MenuDTO menu = menuService.findMenuByMenuCode(menuCode);
+        System.out.println("menu = " + menu);
+    }
+}
