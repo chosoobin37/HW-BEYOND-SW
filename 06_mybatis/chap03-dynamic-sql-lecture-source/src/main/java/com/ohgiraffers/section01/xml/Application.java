bd0ce0ad887bcbe0ab19a7dc46d80e44a6735adb
조수빈 <angelajsb@naver.com>
2024-02-21 10:23:36 +0900

    02/21 MyBatis Dynamic SQL

--- 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ 06_mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -18,6 +18,7 @@ public class Application {
 
             switch (no) {
                 case 1:
+                    ifSubMenu();
                     break;
                 case 2:
                     break;
@@ -31,7 +32,38 @@ public class Application {
                     System.out.println("프로그램을 종료합니다.");
                     return;
             }
-
         } while (true);
+    }
+
+    public static void ifSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+
+        do {
+            System.out.println("===== if SubMenu =====");
+            System.out.println("1. 원하는 금액대에 적합한 추천 메뉴 목록 보여주기");
+            System.out.println("2. 메뉴 이름 혹은 카테고리명으로 검색하여 메뉴 목록 보여주기");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호를 입력해주세요: ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    menuService.findMenuByPrice(inputPrice());
+                    break;
+                case 2:
+                    break;
+                case 9:
+                    return;
+            }
+        } while (true);
+
+    }
+
+    public static int inputPrice() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색할 가격대의 최대 금액을 입력해주세요: ");
+
+        return sc.nextInt();
     }
 }
