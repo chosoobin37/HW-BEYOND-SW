511c1b2527213a9119d273ef15833505fa6b3905
조수빈 <angelajsb@naver.com>
2024-02-22 15:51:16 +0900

    02/21 MyBatisSpring TransActional

---
+++ 07_mybatisspring/chap02-transcactional-lecture-source/src/main/java/com/ohgiraffers/transactional/Chap02TranscactionalLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02TranscactionalLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02TranscactionalLectureSourceApplication.class, args);
+    }
+
+}
