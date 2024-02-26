befce1350850b7758a3413d167ede3b0854eec72
조수빈 <angelajsb@naver.com>
2024-02-26 12:15:18 +0900

    02/26 JPA Persistence Context

--- 08_jpa/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/A_EntityManagerLifeCycleTests.java
+++ 08_jpa/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/A_EntityManagerLifeCycleTests.java
@@ -2,7 +2,8 @@ package com.ohgiraffers.section01;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import org.junit.jupiter.api.BeforeAll;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
 
 public class A_EntityManagerLifeCycleTests {
     private static EntityManagerFactory entityManagerFactory;
@@ -10,6 +11,33 @@ public class A_EntityManagerLifeCycleTests {
 
     @BeforeAll
     public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
 
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @Test
+    public void 엔티티_매니저_팩토리와_매니저_생명주기_확인() {
+        System.out.println("entityManagerFactory.hashCode: " + entityManagerFactory.hashCode());
+        System.out.println("entityManager.hashCode: " + entityManager.hashCode());
+    }
+
+    @Test
+    public void 엔티티_매니저_팩토리와_매니저_생명주기_확인2() {
+        System.out.println("entityManagerFactory.hashCode: " + entityManagerFactory.hashCode());
+        System.out.println("entityManager.hashCode: " + entityManager.hashCode());
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
     }
 }
