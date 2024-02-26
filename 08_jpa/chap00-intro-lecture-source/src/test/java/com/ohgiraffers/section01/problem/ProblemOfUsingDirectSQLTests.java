5ee13ab412ad923f735f9f7f545fd18abf1114be
조수빈 <angelajsb@naver.com>
2024-02-26 10:23:20 +0900

    02/26 JPA Intro

---
+++ 08_jpa/chap00-intro-lecture-source/src/test/java/com/ohgiraffers/section01/problem/ProblemOfUsingDirectSQLTests.java
@@ -0,0 +1,97 @@
+package com.ohgiraffers.section01.problem;
+
+import org.junit.jupiter.api.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemOfUsingDirectSQLTests {
+    private Connection con;
+
+    @BeforeEach
+    void setConnection() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/menudb";
+        String user = "swcamp";
+        String password = "swcamp";
+
+        Class.forName(driver);
+
+        con = DriverManager.getConnection(url, user, password);
+        con.setAutoCommit(false);
+    }
+
+    @AfterEach
+    void closeConnection() throws SQLException {
+        con.rollback();
+        con.close();
+    }
+
+    /* 설명. JDBC API를 이용해 직접 SQL을 다룰 때 발생할 수 있는 문제점
+     *  1) 데이터 변환, SQL 작성, JDBC API 코드 등의 중복 작성 -> 개발 시간 증가, 유지 보수성 저하
+     *  2) SQL에 의존하여 개발 
+     *  3) 패러다임 불일치 -> 상속, 연관관계, 객체 그래프 탐색, 방향성
+     * */
+
+    @DisplayName("직접 SQL을 작성하여 메뉴를 조회할 때 발생하는 문제 확인")
+    @Test
+    void testDirectSelectSQL() throws SQLException {
+
+        // given
+        String query = "SELECT MENU_CODE, MENU_NAME, MENU_PRICE, CATEGORY_CODE,"
+                + "ORDERABLE_STATUS FROM TBL_MENU";
+
+        // when
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+
+        List<Menu> menuList = new ArrayList<>();
+        while (rset.next()) {
+            Menu menu = new Menu();
+            menu.setMenuCode(rset.getInt("MENU_CODE"));
+            menu.setMenuName(rset.getString("MENU_NAME"));
+            menu.setMenuPrice(rset.getInt("MENU_PRICE"));
+            menu.setMenuCategory(rset.getInt("CATEGORY_CODE"));
+            menu.setOrderableStatus(rset.getString("ORDERABLE_STATUS"));
+
+            menuList.add(menu);
+        }
+
+        // then
+        Assertions.assertTrue(!menuList.isEmpty());
+        menuList.forEach(System.out::println);
+
+        rset.close();
+        stmt.close();
+    }
+
+    @DisplayName("직접 SQL을 작성하여 신규 메뉴를 추가할 때 발생하는 문제 확인")
+    @Test
+    void testDirectInsertSQL() throws SQLException {
+
+        // given
+        Menu menu = new Menu();
+        menu.setMenuName("얼그레이다쿠와즈");
+        menu.setMenuPrice(9000);
+        menu.setMenuCategory(1);
+        menu.setOrderableStatus("Y");
+
+        String query = "INSERT INTO TBL_MENU(MENU_NAME, MENU_PRICE, CATEGORY_CODE," +
+                "ORDERABLE_STATUS) VALUES (?, ?, ?, ?)";
+
+        // when
+        PreparedStatement pstmt = con.prepareStatement(query);
+        pstmt.setString(1, menu.getMenuName());
+        pstmt.setInt(2, menu.getMenuPrice());
+        pstmt.setInt(3, menu.getMenuCategory());
+        pstmt.setString(4, menu.getOrderableStatus());
+
+        int result = pstmt.executeUpdate();
+
+        // then
+        Assertions.assertEquals(1, result);
+        pstmt.close();
+
+    }
+}
