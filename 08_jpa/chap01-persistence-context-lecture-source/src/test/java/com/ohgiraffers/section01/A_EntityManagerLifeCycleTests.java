5ce883bf9bc02e41534a7b5bf7c684c8675d2ee0
조수빈 <angelajsb@naver.com>
2024-02-26 11:45:59 +0900

    02/26 JPA Persistence Context

---
+++ 08_jpa/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/A_EntityManagerLifeCycleTests.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.BeforeAll;
+
+public class A_EntityManagerLifeCycleTests {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+
+    }
+}
