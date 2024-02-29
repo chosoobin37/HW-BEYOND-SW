accc8e769bded9dbbcae0536caf9c9dabb81a597
조수빈 <angelajsb@naver.com>
2024-02-29 17:56:54 +0900

    plrecipe_member setting update

--- Plrecipe_project02/plrecipe/plrecipe/src/test/java/com/sixcandoit/plrecipe/feature/group/GroupServiceTests.java
+++ Plrecipe_project02/plrecipe/plrecipe/src/test/java/com/sixcandoit/plrecipe/feature/group/GroupServiceTests.java
@@ -1,7 +1,10 @@
 package com.sixcandoit.plrecipe.feature.group;
 
+import com.sixcandoit.plrecipe.feature.member.MemberDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -20,5 +23,27 @@ public class GroupServiceTests {
         group.forEach(System.out::println);
 
         assertNotNull(group);
+    }
+
+
+    @Test
+    void testSelectMembersByGroupId() {
+        int groupId = 1;
+
+        List<MemberDTO> members = groupService.selectMembersByGroupId(groupId);
+
+        assertNotNull(members);
+
+        for (MemberDTO member : members) {
+            System.out.println(member);
+        }
+
+    @ParameterizedTest
+    @ValueSource(strings={"mfk5gd@daum.net"})
+    void selectPublicOrPrivateGroup(String memberEmail){
+        List<GroupDTO> groupList = groupService.selectPublicOrPrivateGroup(memberEmail);
+        groupList.forEach(System.out::println);
+
+        assertNotNull(groupList);
     }
 }
