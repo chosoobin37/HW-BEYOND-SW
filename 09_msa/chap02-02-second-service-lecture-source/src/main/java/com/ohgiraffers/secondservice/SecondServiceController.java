f7d693263d11810cd156d3429572e430e9cc8de1
조수빈 <angelajsb@naver.com>
2024-03-04 10:52:12 +0900

    eureka service gateway

---
+++ 09_msa/chap02-02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/SecondServiceController.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("health_check")
+    public String healthCheck() {
+        return "I'm OK-ver2";
+    }
+
+    @GetMapping("message")
+    public String message(@RequestHeader("second_request") String header) {
+        return "Second Service Message";
+    }
+}
