a6f923d2d83713574d53c3430f14db85b104a1d3
조수빈 <angelajsb@naver.com>
2024-03-04 15:42:55 +0900

    MSA User Service

---
+++ 09_msa/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.ohgiraffers.userservice.controller;
+
+import com.ohgiraffers.userservice.vo.HelloVO;
+import com.ohgiraffers.userservice.vo.RequestUser;
+import com.ohgiraffers.userservice.vo.ResponseUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private Environment env;
+    private HelloVO helloVO;
+
+    @Autowired
+    public UserController(Environment env, HelloVO helloVO) {
+        this.env = env;
+        this.helloVO = helloVO;
+    }
+
+    /* 설명.
+     *  application.yml 파일로부터 설정값 불러오기 -> 2가지 방법 제공
+     *  1) Environment를 의존성 주입 받아 getProperty로 설정 키 작성해 불러오기
+     *  2) @Value 활용해 필드로 주입 받고 활용
+    * */
+
+    /* 설명. 1) Environment 활용해 설정값 불러오기 -> getProperty */
+    @GetMapping("/health_check")
+    public String status() {
+        return "Server at " + env.getProperty("local.server.port") +
+                ", swcamp.message: " + env.getProperty("swcamp.message");
+    }
+
+    /* 설명. 2) @Value 활용해 설정값 불러오기 -> getter */
+    @GetMapping("do_msa")
+    public String doMSA() {
+        return helloVO.getMessage();
+    }
+
+
+    /* 설명. 회원가입 -> POST - /users */
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> registUser(@RequestBody RequestUser user) {
+        System.out.println("user = " + user);
+
+        ResponseUser responseUser = new ResponseUser();
+        responseUser.setName("응답 재확인");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
