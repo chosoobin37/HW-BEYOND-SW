7a56b900f3db42d71b5330564745fe73a50e33fe
조수빈 <angelajsb@naver.com>
2024-03-26 11:34:22 +0900

    Vur Router

--- 13_vue/08_router/src/main.js
+++ 13_vue/08_router/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import router from './router/01_router.js'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// router 설정을 하기 위해서는 다음과 같이 router 등록 필요
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+  
---
+++ 13_vue/08_router/src/router/01_router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from "@/views/01_router/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HomeView
+    }
+  ]
+})
+
+/*  */
+export default router;
