a1e401a141b11bc80c4f037d52873b23e63570cc
조수빈 <angelajsb@naver.com>
2024-04-08 12:17:52 +0900

    Kubenests Boot Vue

--- 14_devops/vueProject/src/App.vue
+++ 14_devops/vueProject/src/App.vue
@@ -27,7 +27,11 @@ const sendPlus = async() => {
     // await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
 
     // 도커 컨테이너 간에 네트워크 연결 후 (5173/api가 아니라 8011/api로 변경해야 함)
-    await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    // await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    
+    // 백엔드를 쿠버네트스를 활용한 워커노드로 띄웠을 경우 (30001번 NodePort)
+    await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
+
   const data = await response.json();
   result.value = data.sum;    // {sum: 3} 형식으로 반환환
 }
